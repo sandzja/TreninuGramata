d38ec89152bfd8172bc62e1ed76a735c9739e4e3
Signis Vavere <signis.vavere@gmail.com>
2012-03-30 08:11:31 +0300

    fix #18: script change

--- application/sql/data/import.sh
+++ application/sql/data/import.sh
@@ -17,3 +17,14 @@ mysql -u trainingbook --password=zjfDHUZCxNUvet9B trainingbook < treninu_plans_8
 mysql -u trainingbook --password=zjfDHUZCxNUvet9B trainingbook < treninu_plans_9.sql 
 
 mysql -u trainingbook --password=zjfDHUZCxNUvet9B trainingbook < treninu_plans_10.sql 
+
+mysql -u trainingbook --password=zjfDHUZCxNUvet9B trainingbook < treninu_plans_11.sql 
+
+mysql -u trainingbook --password=zjfDHUZCxNUvet9B trainingbook < treninu_plans_12.sql 
+
+mysql -u trainingbook --password=zjfDHUZCxNUvet9B trainingbook < treninu_plans_13.sql 
+
+mysql -u trainingbook --password=zjfDHUZCxNUvet9B trainingbook < treninu_plans_14.sql 
+
+mysql -u trainingbook --password=zjfDHUZCxNUvet9B trainingbook < treninu_plans_15.sql 
+
